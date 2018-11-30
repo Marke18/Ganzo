247fc1ee644213c910e5d5ee601a3d8b6ebd6938
Marke18 <luigi.marchesi01@gmail.com>
2018-11-30 12:41:11 +0100

    Aggiunto Elimina account

--- src/main.js
+++ src/main.js
@@ -36,18 +36,20 @@ var titolo2 = new Vue({
     }
   }
 })
-//eslint-disable-next-line
+// eslint-disable-next-line
 var app1 = new Vue({
   el: '#app-1',
-  data: {
+  data () {
     return {
       // LOGIN
       username: '',
       password: '',
       pag: 0,
+      headerSlot: 'Pinguuuuuuuu',
+      footerSlot: 'Pinguuuuuuuu',
       btnL: {
         float: 'left',
-        marginTop: '4vh',
+        marginTop: '3vh',
         marginLeft: '5vh',
         width: '30%',
         height: '6vh',
@@ -59,19 +61,25 @@ var app1 = new Vue({
       },
       btnR: {
         float: 'right',
-        marginTop: '4vh',
+        marginTop: '3vh',
         marginRight: '5vh',
         width: '30%',
         minHeight: '22px',
         height: '6vh',
         padding: '0vh',
-        textALign: 'center',
         color: '#4f2fcc',
         textDecoration: 'none'
       },
+      bodyCard: {
+        backgroundImage: 'url("https://www.ilgazzettino.it/photos/HIGH/00/24/3300024_1816_pinguino_adelia.jpg")',
+        backgroundSize: 'cover',
+        height: '30vh',
+        margin: '-1.25rem'
+      },
       input: {
         width: '90%',
-        height: '5vh'
+        height: '5vh',
+        marginLeft: '5%'
       },
       // REGISTRATI
       account: {
@@ -96,7 +104,6 @@ var app1 = new Vue({
             }
           }
           if (this.pag !== 2) {
-            alert('Le credenziali inserite sono errate, Riprova')
             this.username = ''
             this.password = ''
           }
@@ -109,17 +116,49 @@ var app1 = new Vue({
     },
     // REGISTRATI
     creaAccount () {
+      // eslint-disable-next-line
       var obj = new db.datausers(this.account)
       obj.save((err, res) => {
-        if (!err){
+        if (!err) {
           this.pag = 2
         } else {
-          alert("Lo username o l'e-mail inseriti appartengono già ad un altro account registrato")
+          this.account.username = ''
+          this.account.password = ''
+          this.account.nome = ''
+          this.account.cognome = ''
+          this.account.email = ''
         }
       })
     },
     pagina0 () {
       this.pag = 0
+    },
+    // HOME
+    cancellaAccount () {
+      var query = {} // get all records
+      var hints = {'$orderby': {'_id': -1}} // top ten, sort by creation id in descending order
+      db.datausers.find(query, hints, (err, res) => {
+        if (!err) {
+          var tro = 0
+          var obj = {}
+          for (var i = 0; i < res.length && tro === 0; i++) {
+            if (res[i].username === this.username || res[i].username === this.account.username) {
+              tro = 1
+              obj = res[i]
+              this.pag = 0
+              obj.delete((err, res) => {
+                if (!err) {
+                }
+              })
+            }
+          }
+          if (this.pag !== 2) {
+            this.username = ''
+            this.password = ''
+          }
+          // res is an array of datausers instances
+        }
+      })
     }
   }
 })
